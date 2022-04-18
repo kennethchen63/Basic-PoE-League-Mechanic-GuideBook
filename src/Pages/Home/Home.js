cbc5ed7e6227a77fb4abd6dbd36e789cffb3607b
kennethchen6399 <kennethchen6399@gmail.com>
2022-04-17 22:28:12 -0400

    Added gray background & updated Material UI

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Box, Grid } from '@material-ui/core';
+import { Box, Grid } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
 import HomeDesc from "./Components/HomeDesc";
 
+const useStyles = makeStyles(theme => ({
+    box: {
+        width: '100%',
+        height: "100%", 
+        backgroundColor: 'gray', 
+    },
+}));
+
 function Home() {
+
+    const classes = useStyles();
+
     return (
         <div>
             <Grid container>
                 <Grid item xs={12}>
-                    <Box>
+                    <Box className={classes.box}>
                         <div>
                             <HomeDesc/>
                         </div>
