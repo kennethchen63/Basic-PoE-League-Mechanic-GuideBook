7882daf78e2c24d6443e82702d3a140323855871
kennethchen6399 <kennethchen6399@gmail.com>
2022-04-25 15:36:13 -0400

    Changed table background color

--- frontend/src/Pages/Synthesis/Components/SynTable.js
+++ frontend/src/Pages/Synthesis/Components/SynTable.js
@@ -1,41 +1,39 @@
 import React from "react";
 import { List, ListItem, Typography, Divider, Box } from "@mui/material";
-import {makeStyles} from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles(theme => ({
-    ring_box: {
-        backgroundColor: 'brown',
-    },
+const useStyles = makeStyles((theme) => ({
+  ring_box: {
+    backgroundColor: "#121212",
+  },
 }));
 
 const data = [
-    { mod: "Herald of X has (40-60%) Buff Effect"},
-    { mod: "(40-60)% increased X Damage while affected by Herald of X"},
-    { mod: "+1% to maximum X Resistance/4% additional Physical Damage Reduction while affected by Herald of X"},
-    { mod: "Herald of X has (40-30)% increased Mana Reservation Efficiency"},
-    { mod: "+(50-60)% to X Resistance while affected by Herald of X"},
-    { mod: "Agony Crawler/Sentinels of Purity deal (70-100%) incresed Damage"},
-]
-
+  { mod: "Herald of X has (40-60%) Buff Effect" },
+  { mod: "(40-60)% increased X Damage while affected by Herald of X" },
+  {
+    mod: "+1% to maximum X Resistance/4% additional Physical Damage Reduction while affected by Herald of X",
+  },
+  { mod: "Herald of X has (40-30)% increased Mana Reservation Efficiency" },
+  { mod: "+(50-60)% to X Resistance while affected by Herald of X" },
+  { mod: "Agony Crawler/Sentinels of Purity deal (70-100%) incresed Damage" },
+];
 
 function SynTable() {
-    
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Box className={classes.ring_box} container>
-            {data.map((data) => (
-                <List>
-                    <ListItem>
-                        <Typography>
-                            {data.mod}
-                        </Typography>
-                    </ListItem>
-                    <Divider/>
-                </List>
-            ))}
-        </Box>
-    )
+  return (
+    <Box className={classes.ring_box} container>
+      {data.map((data) => (
+        <List>
+          <ListItem>
+            <Typography>{data.mod}</Typography>
+          </ListItem>
+          <Divider />
+        </List>
+      ))}
+    </Box>
+  );
 }
 
 export default SynTable;
